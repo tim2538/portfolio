91dcb0d6e50f3e9ef04d4d1dd21a228e40f76132
tim2538 <chawakorn_fb@hotmail.com>
2023-01-27 23:38:44 +0700

    Prepare component and page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
 export default function App() {
   return (
-    <h1>Hello React</h1>
+    <div>
+      <h1>Hello React</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { CssBaseline, CssVarsProvider } from '@mui/joy';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <CssVarsProvider>
+        <CssBaseline />
+        <App />
+      </CssVarsProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
