d26603de6db69a2d12cb909b6cf0fd5a89d9205e
tim2538 <chawakorn_fb@hotmail.com>
2023-04-30 01:04:32 +0700

    Update

--- src/common/constants/index.ts
+++
@@ -1,1 +0,0 @@
-export const COPYRIGHT = `Copyright © ${new Date().getFullYear()} by Chawakorn T., All Rights Reserved.`;
--- src/common/constants/menus.ts
+++
@@ -1,5 +0,0 @@
-export const MENUS = [
-  { name: 'Home', to: '/' },
-  { name: 'Portfolio', to: '/portfolio' },
-  { name: 'About', to: '/about' }
-];
