bc17917883c67baf05f6cb462485c83f27008b6e
tim2538 <chawakorn_fb@hotmail.com>
2023-04-30 13:37:47 +0700

    Add background for navbar

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -36,114 +36,129 @@ export default function Navbar() {
   }
 
   return (
-    <AppBar
-      color="transparent"
-      sx={(theme) => ({
-        borderBottomLeftRadius: { xs: open ? 16 : 0, sm: 0 },
-        borderBottomRightRadius: { xs: open ? 16 : 0, sm: 0 },
-        backgroundColor: {
-          xs:
-            open || trigger
+    <>
+      <AppBar
+        color="transparent"
+        sx={(theme) => ({
+          borderBottomLeftRadius: { xs: open ? 16 : 0, sm: 0 },
+          borderBottomRightRadius: { xs: open ? 16 : 0, sm: 0 },
+          backgroundColor: {
+            xs:
+              open || trigger
+                ? alpha(theme.palette.background.paper, 0.5)
+                : 'transparent',
+            sm: trigger
               ? alpha(theme.palette.background.paper, 0.5)
-              : 'transparent',
-          sm: trigger
-            ? alpha(theme.palette.background.paper, 0.5)
-            : 'transparent'
-        },
-        boxShadow: {
-          xs: open || trigger ? '0px 3px 10px rgba(0, 0, 0, 0.15)' : 'none',
-          sm: trigger ? '0px 3px 10px rgba(0, 0, 0, 0.15)' : 'none'
-        },
-        backdropFilter: 'blur(8px)',
-        transition:
-          'border-radius 0.15s ease-in-out, background-color 0.5s ease-out, box-shadow 0.5s ease-out'
-      })}
-    >
-      <Container>
-        <Toolbar disableGutters sx={{ my: 1 }}>
-          <Typography
-            component={RRLink}
-            to="/"
-            variant="h5"
-            color="inherit"
-            sx={{ textDecoration: 'none' }}
-          >
-            {pathname === '/' ? 'WELCOME' : 'tim2538'}
-          </Typography>
-          <Box flexGrow={1} />
-          <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
-            {MENUS.map(({ name, to }) => (
-              <Button
-                key={name}
-                component={RRLink}
-                to={to}
-                color={pathname === to ? 'primary' : 'inherit'}
-                disableRipple
-                sx={{
-                  borderRadius: 2,
-                  ml: 0.5,
-                  '&:hover, &.Mui-focusVisible': {
-                    bgcolor: 'transparent',
-                    color: 'primary.main'
-                  }
-                }}
-              >
-                {name}
-              </Button>
-            ))}
-          </Box>
-          <Tooltip
-            title={
-              mode === 'light' ? 'Turn off the light' : 'Turn on the light'
-            }
-          >
-            <IconButton
-              onClick={() => toggleColorMode()}
-              sx={{ color: 'inherit', ml: 0.5 }}
+              : 'transparent'
+          },
+          boxShadow: {
+            xs: open || trigger ? '0px 3px 10px rgba(0, 0, 0, 0.15)' : 'none',
+            sm: trigger ? '0px 3px 10px rgba(0, 0, 0, 0.15)' : 'none'
+          },
+          backdropFilter: 'blur(8px)',
+          transition:
+            'border-radius 0.15s ease-in-out, background-color 0.5s ease-out, box-shadow 0.5s ease-out'
+        })}
+      >
+        <Container>
+          <Toolbar disableGutters sx={{ my: 1 }}>
+            <Typography
+              component={RRLink}
+              to="/"
+              variant="h5"
+              color="inherit"
+              sx={{ textDecoration: 'none' }}
             >
-              {mode === 'light' ? <DarkModeOutlined /> : <LightModeRounded />}
+              {pathname === '/' ? 'WELCOME' : 'tim2538'}
+            </Typography>
+            <Box flexGrow={1} />
+            <Box sx={{ display: { xs: 'none', sm: 'flex' } }}>
+              {MENUS.map(({ name, to }) => (
+                <Button
+                  key={name}
+                  component={RRLink}
+                  to={to}
+                  color={pathname === to ? 'primary' : 'inherit'}
+                  disableRipple
+                  sx={{
+                    borderRadius: 2,
+                    ml: 0.5,
+                    '&:hover, &.Mui-focusVisible': {
+                      bgcolor: 'transparent',
+                      color: 'primary.main'
+                    }
+                  }}
+                >
+                  {name}
+                </Button>
+              ))}
+            </Box>
+            <Tooltip
+              title={
+                mode === 'light' ? 'Turn off the light' : 'Turn on the light'
+              }
+            >
+              <IconButton
+                onClick={() => toggleColorMode()}
+                sx={{ color: 'inherit', ml: 0.5 }}
+              >
+                {mode === 'light' ? <DarkModeOutlined /> : <LightModeRounded />}
+              </IconButton>
+            </Tooltip>
+            <IconButton
+              onClick={handleMenu}
+              sx={{ display: { xs: 'flex', sm: 'none' }, ml: 0.5 }}
+            >
+              <Hamburger open={open} />
             </IconButton>
-          </Tooltip>
-          <IconButton
-            onClick={handleMenu}
-            sx={{ display: { xs: 'flex', sm: 'none' }, ml: 0.5 }}
-          >
-            <Hamburger open={open} />
-          </IconButton>
-        </Toolbar>
-        <Collapse sx={{ display: { xs: 'block', sm: 'none' } }} in={open}>
-          <Divider />
-          <Toolbar sx={{ display: 'block' }}>
-            <MenuList>
-              {MENUS.map(({ name, to }) => {
-                return (
-                  <MenuItem
-                    key={name}
-                    component={RRLink}
-                    to={to}
-                    onClick={handleMenu}
-                    disableRipple
-                    sx={{
-                      mx: -2,
-                      px: 1.5,
-                      borderRadius: 2,
-                      fontWeight: 500,
-                      justifyContent: 'center',
-                      color: pathname === to ? 'primary.main' : 'text.primary',
-                      '&:hover, &.Mui-focusVisible': {
-                        bgcolor: 'transparent',
-                        color: 'primary.main'
-                      }
-                    }}
-                  >
-                    {name}
-                  </MenuItem>
-                );
-              })}
-            </MenuList>
           </Toolbar>
-        </Collapse>
-      </Container>
-    </AppBar>
+          <Collapse sx={{ display: { xs: 'block', sm: 'none' } }} in={open}>
+            <Divider />
+            <Toolbar sx={{ display: 'block' }}>
+              <MenuList>
+                {MENUS.map(({ name, to }) => {
+                  return (
+                    <MenuItem
+                      key={name}
+                      component={RRLink}
+                      to={to}
+                      onClick={handleMenu}
+                      disableRipple
+                      sx={{
+                        mx: -2,
+                        px: 1.5,
+                        borderRadius: 2,
+                        fontWeight: 500,
+                        justifyContent: 'center',
+                        color:
+                          pathname === to ? 'primary.main' : 'text.primary',
+                        '&:hover, &.Mui-focusVisible': {
+                          bgcolor: 'transparent',
+                          color: 'primary.main'
+                        }
+                      }}
+                    >
+                      {name}
+                    </MenuItem>
+                  );
+                })}
+              </MenuList>
+            </Toolbar>
+          </Collapse>
+        </Container>
+      </AppBar>
+      <Box
+        onClick={handleMenu}
+        sx={(theme) => ({
+          width: '100%',
+          height: '100vh',
+          position: 'fixed',
+          zIndex: theme.zIndex.appBar - 1,
+          display: { xs: open ? 'block' : 'none', sm: 'none' },
+          backgroundColor: 'background.paper',
+          opacity: 0.5
+        })}
+      />
+    </>
   );
 }
