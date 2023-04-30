1e5ec728d1fa801b77638911a7223a4db6f51d1e
tim2538 <chawakorn_fb@hotmail.com>
2023-04-30 13:52:24 +0700

    Add HamburgerV2 component

--- src/components/Hamburger/index.tsx
+++ src/components/Hamburger/index.tsx
@@ -60,3 +60,66 @@ export default function Hamburger({ open }: HamburgerProps) {
     </Box>
   );
 }
+
+export function HamburgerV2({ open }: HamburgerProps) {
+  return (
+    <Box
+      sx={{
+        width: 24,
+        height: 24,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+      }}
+    >
+      <Box
+        sx={{
+          width: 18,
+          height: 2,
+          position: 'relative'
+        }}
+      >
+        <Box
+          sx={{
+            width: 9,
+            height: 2,
+            bgcolor: 'text.primary',
+            borderRadius: 2,
+            position: 'absolute',
+            top: -5,
+            transform: open
+              ? 'translateX(15%) translateY(100%) rotate(-135deg)'
+              : 'none',
+            transition: 'transform 0.4s'
+          }}
+        />
+        <Box
+          sx={{
+            width: 18,
+            height: 2,
+            bgcolor: 'text.primary',
+            borderRadius: 2,
+            position: 'absolute',
+            transform: open ? 'rotate(-45deg)' : 'none',
+            transition: 'transform 0.4s'
+          }}
+        />
+        <Box
+          sx={{
+            width: 9,
+            height: 2,
+            bgcolor: 'text.primary',
+            borderRadius: 2,
+            position: 'absolute',
+            top: 5,
+            right: 0,
+            transform: open
+              ? 'translateX(-15%) translateY(-100%) rotate(-135deg)'
+              : 'none',
+            transition: 'transform 0.4s'
+          }}
+        />
+      </Box>
+    </Box>
+  );
+}
