e507c0a67a8373ccdc966dd92fbae235741b596a
tim2538 <chawakorn_fb@hotmail.com>
2023-05-02 18:22:43 +0700

    Add album images

---
+++ src/common/constants/albums.ts
@@ -0,0 +1,82 @@
+export const ALBUMS = [
+  {
+    id: 1,
+    name: 'Landscape',
+    path: 'landscape',
+    filename: 'landscape.jpeg',
+    width: 2000,
+    height: 1333
+  },
+  {
+    id: 2,
+    name: 'Cityscape',
+    path: 'cityscape',
+    filename: 'cityscape.jpeg',
+    width: 2000,
+    height: 1500
+  },
+  {
+    id: 3,
+    name: 'Black and White',
+    path: 'black-and-white',
+    filename: 'black-and-white.jpeg',
+    width: 1333,
+    height: 2000
+  },
+  {
+    id: 4,
+    name: 'People',
+    path: 'people',
+    filename: 'people.jpeg',
+    width: 2000,
+    height: 1333
+  },
+  {
+    id: 5,
+    name: 'Graduation',
+    path: 'graduation',
+    filename: 'graduation.jpeg',
+    width: 2000,
+    height: 1333
+  },
+  {
+    id: 6,
+    name: 'Event',
+    path: 'event',
+    filename: 'event.jpeg',
+    width: 1333,
+    height: 2000
+  },
+  {
+    id: 7,
+    name: 'Packshot',
+    path: 'packshot',
+    filename: 'packshot.jpeg',
+    width: 1333,
+    height: 2000
+  },
+  {
+    id: 8,
+    name: 'Frame in Frame',
+    path: 'frame-in-frame',
+    filename: 'frame-in-frame.jpeg',
+    width: 1333,
+    height: 2000
+  },
+  {
+    id: 9,
+    name: 'Downsizing',
+    path: 'downsizing',
+    filename: 'downsizing.jpeg',
+    width: 2000,
+    height: 1334
+  },
+  {
+    id: 10,
+    name: 'Pre-wedding',
+    path: 'pre-wedding',
+    filename: 'pre-wedding.jpeg',
+    width: 1333,
+    height: 2000
+  }
+];
--- src/pages/Album/index.tsx
+++ src/pages/Album/index.tsx
@@ -1,14 +1,133 @@
-import { Container } from '@mui/material';
+import { KeyboardArrowRightRounded } from '@mui/icons-material';
+import {
+  Box,
+  Button,
+  CardMedia,
+  Container,
+  Grid,
+  Theme,
+  Typography,
+  useMediaQuery
+} from '@mui/material';
+import { Link as RRLink } from 'react-router-dom';
 import { COPYRIGHT } from '../../common/constants';
+import { ALBUMS } from '../../common/constants/albums';
 import AnimatedBox from '../../components/AnimatedBox';
 import Title from '../../components/Title';
 
+interface Item {
+  id: number;
+  name: string;
+  path: string;
+  filename: string;
+  width: number;
+  height: number;
+}
+
 export default function Album() {
+  const isSmUpScreen = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.up('sm')
+  );
+  const isMdUpScreen = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.up('md')
+  );
+  const isLgUpScreen = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.up('lg')
+  );
+
+  const maxWidthHeight = (function () {
+    if (isLgUpScreen) return 500;
+    if (isMdUpScreen) return 400;
+    if (isSmUpScreen) return 500;
+    return 400;
+  })();
+
+  function GridLayout(items: Item[]) {
+    return (
+      <Grid
+        container
+        rowSpacing={{ xs: 20, md: 16 }}
+        columnSpacing={{ xs: 0, md: 4 }}
+        sx={{ mb: 3 }}
+      >
+        {items.map((item, index) => {
+          const isHorizontalImage = item.width >= item.height;
+
+          return (
+            <Grid
+              item
+              key={index}
+              xs={12}
+              md={6}
+              sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center'
+              }}
+            >
+              <AnimatedBox
+                direction={index % 2 === 0 ? 'left' : 'right'}
+                sx={{
+                  textAlign: 'center'
+                }}
+              >
+                <CardMedia
+                  component={RRLink}
+                  to={item.path}
+                  image={require(`../../assets/album/main/${item.filename}`)}
+                  title={item.name}
+                  sx={{
+                    borderRadius: 1,
+                    boxShadow: '0px 5px 20px rgba(0, 0, 0, 0.3)',
+                    width: isHorizontalImage
+                      ? maxWidthHeight
+                      : (item.width / item.height) * maxWidthHeight,
+                    height: isHorizontalImage
+                      ? (item.height / item.width) * maxWidthHeight
+                      : maxWidthHeight
+                  }}
+                />
+                <Box mt={5} sx={{ display: 'flex', flexDirection: 'column' }}>
+                  <Typography
+                    component={RRLink}
+                    to={item.path}
+                    variant="h5"
+                    fontWeight={300}
+                    color="inherit"
+                    sx={{ textDecoration: 'none' }}
+                  >
+                    {item.name.toUpperCase()}
+                  </Typography>
+                  <Button
+                    disableRipple
+                    component={RRLink}
+                    to={item.path}
+                    size="small"
+                    endIcon={<KeyboardArrowRightRounded />}
+                    sx={{
+                      '&:hover, &.Mui-focusVisible': {
+                        bgcolor: 'transparent',
+                        color: 'primary.dark'
+                      }
+                    }}
+                  >
+                    SEE MORE
+                  </Button>
+                </Box>
+              </AnimatedBox>
+            </Grid>
+          );
+        })}
+      </Grid>
+    );
+  }
+
   return (
     <Container>
       <AnimatedBox>
         <Title text="Album" subTitle={COPYRIGHT} />
       </AnimatedBox>
+      <Box sx={{ my: { xs: 16, md: 8 } }}>{GridLayout(ALBUMS)}</Box>
     </Container>
   );
 }
