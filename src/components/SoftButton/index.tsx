b87fa4e0e4e6a3ce134d500b84110f42af306f56
tim2538 <chawakorn_fb@hotmail.com>
2023-05-04 16:14:40 +0700

    Update Soft button component

--- src/components/SoftButton/index.tsx
+++ src/components/SoftButton/index.tsx
@@ -1,11 +1,26 @@
 import { Button, Theme, alpha, useTheme } from '@mui/material';
 import React from 'react';
 
+interface StyledButtonProps {
+  borderRadius: number | string;
+}
+
 export default function SoftButton(props: React.ComponentProps<typeof Button>) {
   const theme = useTheme<Theme>();
   const { color = 'primary', sx, children, ...other } = props;
+
+  // Set color of Button
   let selectedColor = theme.palette.primary;
   if (color !== 'inherit') selectedColor = theme.palette[color];
+  // Set border-radius of Button
+  let { borderRadius } = theme.components?.MuiButton?.styleOverrides
+    ?.root as StyledButtonProps;
+  if ((sx as StyledButtonProps).borderRadius)
+    borderRadius = (sx as StyledButtonProps).borderRadius;
+  if (typeof borderRadius === 'number')
+    borderRadius = `${
+      theme.shape.borderRadius * Math.min(borderRadius, 999)
+    }px`;
 
   return (
     <Button
@@ -24,12 +39,12 @@ export default function SoftButton(props: React.ComponentProps<typeof Button>) {
           right: 0,
           bottom: 0,
           backgroundColor: 'background.paper',
-          borderRadius: '48px',
+          borderRadius,
           zIndex: -1
         },
-        bgcolor: (theme) => alpha(selectedColor.main, 0.2),
+        bgcolor: alpha(selectedColor.main, 0.2),
         '&:hover, &.Mui-focusVisible': {
-          bgcolor: (theme) => alpha(selectedColor.main, 0.4)
+          bgcolor: alpha(selectedColor.main, 0.4)
         }
       }}
     >
